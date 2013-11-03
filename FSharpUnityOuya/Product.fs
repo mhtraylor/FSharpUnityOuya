acec889b1dd232dd3e702e6ffbc953084f9bdc1b
Matthew Traylor <mhtraylor@sarissagamestudio.com>
2013-11-03 12:27:09 -0600

    Added JSON module and data contract.

--- FSharpUnityOuya/Product.fs
+++ FSharpUnityOuya/Product.fs
@@ -16,29 +16,64 @@
 
 //    Product.fs:  Module for Ouya IAP types.
 
+open System
+open System.IO
+open System.Runtime.Serialization
+open System.Runtime.Serialization.Json
+open System.Text
+open System.Xml
+
 type Purchasable = Purchasable of string
 
+[<DataContract>]
 type Product =
     {
+        [<field: DataMember(Name = "currencyCode")>]
         currencyCode : string
+        [<field: DataMember(Name = "identifier")>]
         identifier : string
+        [<field: DataMember(Name = "localPrice")>]
         localPrice : single
+        [<field: DataMember(Name = "name")>]
         name : string
+        [<field: DataMember(Name = "priceInCents")>]
         priceInCents : int
+        [<field: DataMember(Name = "productVersionToBundle")>]
         productVersionToBundle : int
     }
 
+[<DataContract>]
 type Receipt =
     {
+        [<field: DataMember(Name = "currency")>]
         currency : string
+        [<field: DataMember(Name = "gamer")>]
         gamer : string
+        [<field: DataMember(Name = "generatedDate")>]
         generatedDate : System.DateTime
+        [<field: DataMember(Name = "identifier")>]
         identifier : string
+        [<field: DataMember(Name = "localPrice")>]
         localPrice : single
+        [<field: DataMember(Name = "priceInCents")>]
         priceInCents : int
+        [<field: DataMember(Name = "purchaseDate")>]
         purchaseDate : System.DateTime
         uuid : string
     }
+
+module Json =
+
+    let toJson<'a> (ob:'a) =
+        use m = new MemoryStream()
+        (DataContractJsonSerializer(typeof<'a>)).WriteObject(m,ob) 
+        Encoding.Default.GetString(m.ToArray())
+
+    let fromJson<'a> (str:string) =
+        use m = new MemoryStream(str |> Encoding.UTF8.GetBytes)
+        (DataContractJsonSerializer(typeof<'a>)).ReadObject(m)
+        :?> 'a 
+
 
 
 
