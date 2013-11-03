b5935e03c2db06486dce6d6a9cea0e85f8e832c4
Matthew Traylor <mhtraylor@sarissagamestudio.com>
2013-11-03 01:49:49 -0600

    Refactored JNI calls in Java module; added platform check.

--- FSharpUnityOuya/Java.fs
+++ FSharpUnityOuya/Java.fs
@@ -15,94 +15,111 @@
 //    limitations under the License.
 
 //    Java.fs:  Module for Ouya-specific Android JNI functions.
-
+open FSharp.Ouya.Product
 open UnityEngine
 
-do 
-    AndroidJNI.AttachCurrentThread() |> ignore
+// Initialization.
+do
+    if 
+        Application.platform <> RuntimePlatform.Android || Application.isEditor
+    then
+        ()
+    else 
+        AndroidJNI.AttachCurrentThread() |> ignore
 
-    let cls = AndroidJNI.FindClass(name="com/unity3d/player/UnityPlayer")
-    let fid = AndroidJNI.GetStaticFieldID(cls,"currentActivity","Landroid/app/Activity;")
-    let oba = AndroidJNI.GetStaticObjectField(cls,fid)
+        let cls = AndroidJNI.FindClass(name="com/unity3d/player/UnityPlayer")
+        let fid = AndroidJNI.GetStaticFieldID(cls,"currentActivity","Landroid/app/Activity;")
+        let oba = AndroidJNI.GetStaticObjectField(cls,fid)
 
-    let cls = AndroidJNI.FindClass("tv/ouya/sdk/OuyaUnityPlugin")
-    let mid = AndroidJNI.GetMethodID(cls,"<init>","(Landroid/app/Activity;)")
-    let obc = AndroidJNI.NewObject(cls,mid,[| new jvalue(l = oba) |])
+        let cls = AndroidJNI.FindClass("tv/ouya/sdk/OuyaUnityPlugin")
+        let mid = AndroidJNI.GetMethodID(cls,"<init>","(Landroid/app/Activity;)")
+        let obc = AndroidJNI.NewObject(cls,mid,[| new jvalue(l = oba) |])
 
-    Debug.Log("JavaClass: " + obc.ToString())
+        Debug.Log("JavaClass: " + obc.ToString())
 
-let init id =
-    AndroidJNI.AttachCurrentThread() |> ignore
-    AndroidJNI.PushLocalFrame(0) |> ignore
-    try
-        try
-            use jc = new AndroidJavaClass("tv.ouya.sdk.OuyaUnityPlugin")
-            jc.CallStatic<string>("setDeveloperID",[| id + "\0" |]) |> ignore
-            jc.CallStatic("unityInitialized") |> ignore
-        with
-            | _ as e -> Debug.LogError(sprintf "FSharp.Ouya.Java initialization error: %A" e)
-    finally
-        AndroidJNI.PopLocalFrame(System.IntPtr.Zero) |> ignore
+[<Literal>] 
+let JAVA_CLASS = "tv.ouya.sdk.OuyaUnityPlugin"
 
-let setResolution id =
+// AndroidJNI wrapper.
+let tryCall jc f =
     AndroidJNI.AttachCurrentThread() |> ignore
     AndroidJNI.PushLocalFrame(0) |> ignore
     try
         try
-            use jc = new AndroidJavaClass("tv.ouya.sdk.OuyaUnityPlugin")
-            jc.CallStatic("setResolution", [| id + "\0" |]) |> ignore
+            use jc = new AndroidJavaClass(jc)
+            f jc
         with
-            | _ as e -> Debug.LogError(sprintf "FSharp.Ouya.Java unable to set resolution: %A" e)
+            | _ as e -> Debug.LogError(sprintf "FSharp.Ouya.Java error: %A" e)
     finally
         AndroidJNI.PopLocalFrame(System.IntPtr.Zero) |> ignore
 
-let showCursor (b:bool) =
+let tryCallReturn<'a> jc f =
     AndroidJNI.AttachCurrentThread() |> ignore
     AndroidJNI.PushLocalFrame(0) |> ignore
+    let mutable rtn = Unchecked.defaultof<'a>
     try
         try
-            use jc = new AndroidJavaClass("tv.ouya.sdk.OuyaUnityPlugin")
-            jc.CallStatic("showCursor",[| b.ToString() |]) |> ignore
+            use jc = new AndroidJavaClass(jc)
+            rtn <- f jc
         with
-            | _ as e -> Debug.LogError(sprintf "FSharp.Ouya.Java unable to show cursor: %A" e)
+            | _ as e -> Debug.LogError(sprintf "FSharp.Ouya.Java error: %A" e)
     finally
         AndroidJNI.PopLocalFrame(System.IntPtr.Zero) |> ignore
+    rtn
 
-let putGameData (k,v) =
-    AndroidJNI.AttachCurrentThread() |> ignore
-    AndroidJNI.PushLocalFrame(0) |> ignore
-    try
-        try
-            use jc = new AndroidJavaClass("tv.ouya.sdk.OuyaUnityPlugin")
-            jc.CallStatic("putGameData",[| k + "\0"; v + "\0" |]) |> ignore
-        with
-            | _ as e -> Debug.LogError(sprintf "FSharp.Ouya.Java unable to put game data: %A" e)
-    finally
-        AndroidJNI.PopLocalFrame(System.IntPtr.Zero) |> ignore
+let init id = 
+    let f id (jc:AndroidJavaClass) =
+        jc.CallStatic<string>("setDeveloperID",[| id + "\0" |]) |> ignore
+        jc.CallStatic("unityInitialized") |> ignore
+    tryCall JAVA_CLASS (f id)
 
-let getGameData k =
-    AndroidJNI.AttachCurrentThread() |> ignore
-    AndroidJNI.PushLocalFrame(0) |> ignore
-    let mutable r = ""
-    try
-        try
-            use jc = new AndroidJavaClass("tv.ouya.sdk.OuyaUnityPlugin")
-            r <- jc.CallStatic<string>("getGameData",[| k + "\0" |])
-        with
-            | _ as e -> Debug.LogError(sprintf "FSharp.Ouya.Java unable to get game data: %A" e)
-    finally
-        AndroidJNI.PopLocalFrame(System.IntPtr.Zero) |> ignore
-    r
+let setResolution id = 
+    let f id (jc:AndroidJavaClass) =
+        jc.CallStatic("setResolution",[| id + "\0" |])
+    tryCall JAVA_CLASS (f id)
 
-let fetchGamerUUID () =
-    AndroidJNI.AttachCurrentThread() |> ignore
-    AndroidJNI.PushLocalFrame(0) |> ignore
-    try
-        try
-            use jc = new AndroidJavaClass("tv.ouya.sdk.OuyaUnityPlugin")
-            jc.CallStatic("fetchGamerUUID")
-        with
-            | _ as e -> Debug.LogError(sprintf "FSharp.Ouya.Java unable to fetch Gamer UUID: %A" e)
-    finally
-        AndroidJNI.PopLocalFrame(System.IntPtr.Zero) |> ignore
+let showCursor (b:bool) = 
+    let f (b:bool) (jc:AndroidJavaClass) =
+        jc.CallStatic("showCursor",[| b.ToString() |])
+    tryCall JAVA_CLASS (f b)
 
+let putGameData (k,v) = 
+    let f (k,v) (jc:AndroidJavaClass) =
+        jc.CallStatic("putGameData",[| k + "\0"; v + "\0" |])
+    tryCall JAVA_CLASS (f (k,v))
+
+let getGameData k = 
+    let f k (jc:AndroidJavaClass) =
+        jc.CallStatic<string>("getGameData",[| k + "\0" |])
+    tryCallReturn<string> JAVA_CLASS (f k)
+
+let fetchGamerUUID () = 
+    let f (jc:AndroidJavaClass) = 
+        jc.CallStatic("fetchGamerUUID")
+    tryCall JAVA_CLASS (f)
+
+let addGetProduct (Purchasable pid) =
+    let f (pid:string) (jc:AndroidJavaClass) =
+        jc.CallStatic("addGetProduct",pid)
+    tryCall JAVA_CLASS (f pid)
+
+let debugGetProductList () =
+    let f (jc:AndroidJavaClass) =
+        jc.CallStatic("debugGetProductList")
+    tryCall JAVA_CLASS (f)
+
+let clearGetProductList () =
+    let f (jc:AndroidJavaClass) =
+        jc.CallStatic("clearGetProductList")
+    tryCall JAVA_CLASS (f)
+
+let getProductsAsync () =
+    let f (jc:AndroidJavaClass) =
+        jc.CallStatic("getProductsAsync")
+    tryCall JAVA_CLASS (f)
+
+let requestPurchaseAsync (Purchasable pid) =
+    let f (pid:string) (jc:AndroidJavaClass) =
+        jc.CallStatic<string>("requestPurchaseAsync",[| pid + "\0" |]) 
+        |> ignore
+    tryCall JAVA_CLASS (f pid)
