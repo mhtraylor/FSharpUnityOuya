f1b1665fde25b89b6ffe4109caf30176a59e8e10
Matthew Traylor <mhtraylor@sarissagamestudio.com>
2013-11-03 22:27:21 -0600

    Added basic IAP event system.

--- FSharpUnityOuya/OuyaGameObject.fs
+++ FSharpUnityOuya/OuyaGameObject.fs
@@ -17,26 +17,95 @@
 //    OuyaGameObject.fs:  MonoBehaviour for interfacing with Android.
 
 open FSharp.Ouya.Control
+open FSharp.Ouya.Product
+open FSharp.Ouya.Util
 open UnityEngine
 
 type OuyaGameObject () =
     inherit MonoBehaviour ()
 
+    // Public fields. These are exposed to the Unity Editor Inspector.
     [<DefaultValue>]
     val mutable public developerID : string
 
-    // Event handling.
+    // Private backing stores. Mutable!
+    let mutable products = List.empty<Product>
+    let mutable receipts = List.empty<Receipt>
+
+    // Menu event triggers.
     member x.onMenuButtonUp () =
-        OuyaEvent.fire (MenuButton)
+        Menu.trigger (Menu.Button)
 
     member x.onMenuAppearing () =
-        OuyaEvent.fire (MenuAppear)
+        Menu.trigger (Menu.Appearing)
 
     member x.onPause () =
-        OuyaEvent.fire (Pause)
+        Menu.trigger (Menu.Pause)
 
     member x.onResume () =
-        OuyaEvent.fire (Resume)
+        Menu.trigger (Menu.Resume)
+
+    // IAP event triggers.
+    member x.FetchGamerUUIDSuccessListener json =
+        json |> GamerUUID |> IAP.Success |> IAP.trigger
+
+    member x.FetchGamerUUIDFailureListener json =
+        json |> Json.fromJson<Json.Error> |> IAP.Failure |> IAP.trigger
+
+    member x.FetchGamerUUIDCancelListener json =
+        IAP.Cancel |> IAP.trigger
+
+    member x.ProductListClearListener json =
+        [] |> ProductList |> IAP.Success |> IAP.trigger
+
+    member x.ProductListListener = function
+        | NullOrEmpty json ->
+            products <- Json.fromJson<Product> json :: products 
+            products 
+            |> ProductList 
+            |> IAP.Success
+            |> IAP.trigger
+        | _ -> Debug.LogError("OuyaGameObject: received empty json data.")
+
+    member x.ProductFailureListener json =
+        json |> Json.fromJson<Json.Error> |> IAP.Failure |> IAP.trigger
+
+    member x.ProductCompleteListener json =
+        products |> ProductList |> IAP.Success |> IAP.trigger
+
+    member x.PurchaseSuccessListener json =
+        json |> Json.fromJson<Product> |> SingleProduct |> IAP.Success |> IAP.trigger
+
+    member x.PurchaseFailureListener json =
+        json |> Json.fromJson<Json.Error> |> IAP.Failure |> IAP.trigger
+
+    member x.PurchaseCancelListener json =
+        IAP.Cancel |> IAP.trigger
+
+    member x.ReceiptListClearListener json =
+        [] |> ReceiptList |> IAP.Success |> IAP.trigger
+
+    member x.ReceiptListListener = function
+        | NullOrEmpty json ->
+            receipts <- Json.fromJson<Receipt> json :: receipts
+            receipts
+            |> ReceiptList
+            |> IAP.Success
+            |> IAP.trigger
+        | _ -> Debug.LogError("OuyaGameObject: received empty json data")
+
+    member x.ReceiptListFailureListener json =
+        json |> Json.fromJson<Json.Error> |> IAP.Failure |> IAP.trigger
+
+    member x.ReceiptListCompleteListener json =
+        receipts |> ReceiptList |> IAP.Success |> IAP.trigger
+
+    member x.ReceiptListCancelListener json =
+        IAP.Cancel |> IAP.trigger
+        
+    // Initialization.
+    member x.RequestUnityAwake json =
+        Java.init x.developerID        
 
     // Unity callbacks.
     member x.Start () =
